1d7b4a7504309f2ea2a30ac385718fd40c792dab
Pusher Robot <jack@semantic-ui.com>
2015-02-24 14:33:33 -0500

    Updated component to version 1.10.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-ad',
   summary : 'Semantic UI - Ad: Single component release',
-  version : '1.10.1',
+  version : '1.10.2',
   git     : 'git://github.com/Semantic-Org/UI-Ad.git',
 });
 
